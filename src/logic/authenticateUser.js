16e2a984f0234eb1ba3c4891e3b9ea27b56ba309
vikrant <vikrantyadav4802@gmail.com>
2024-08-07 14:34:59 +0530

    jwt auth added

--- src/logic/authenticateUser.js
+++ src/logic/authenticateUser.js
@@ -1,12 +1,22 @@
 const {userData} = require("../mongodb")
-
-async function authenticateUser(user){
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+require('dotenv').config()
+async function authenticateUser(user,req,res){
     
     try{
         const findUser = await userData.findOne({username:user.username})
-        if(!findUser || user.password!=findUser.password || user.teamCode!=findUser.teamCode){
+        if(!findUser) return {success:false, message:"not a user"}
+
+        const isMatch = await bcrypt.compare(user.password, findUser.password)
+
+        if(!isMatch || user.teamCode!=findUser.teamCode){
             return {success:false,message:"invalid creadentials"}
         }
+
+        const token = jwt.sign({username : user.username, teamCode : user.teamCode},process.env.SECRET_KEY)
+        res.cookie('token', token,{httpOnly: true, secure : true, sameSite:'none'})
+
         return {success:true,message:"valid credentials"}
     }
     catch(error){
