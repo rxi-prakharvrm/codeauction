385fb3913547def46ab731fb6d6b3d0ae581001c
Prakhar Verma <prakharverma1022@gmail.com>
2024-09-01 21:15:38 +0530

    remove unnecessary code

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ var bidData = { index: null, title: "", teamCode: null, amount: 0 };
 
 io.on('connection', (socket) => {
     socket.on('host', async (index) => {
-
         var idx = Number(index)
 
         bidData.index = idx
@@ -34,19 +33,18 @@ io.on('connection', (socket) => {
         var title = 'Waiting for problem...';
         var tag = null
         var desc = "No problem in bid"
+
         if (question != null) {
             title = question.title
             tag = question.tag
             desc = question.desc
             bidData.title = title
-        }
-     
+        }     
 
         io.emit('question', title, desc, tag);
     })
 
-    socket.on('bid', (teamCode, amount) => {
-        
+    socket.on('bid', (teamCode, amount) => {        
         if (Number(amount) > bidData.amount) {
             bidData.teamCode = teamCode;
             bidData.amount = amount
@@ -71,15 +69,11 @@ io.on('connection', (socket) => {
         console.log("1");
         await questionBank.updateOne({ index: bidData.index}, { $set: { owner: bidData.teamCode } })
         const list= await questionBank.find({})
-        // const newList=list.filter(question=>question.owner!=null)
      
         try {
-            console.log("2");
             const team_Data=await teamData.findOne({team:bidData.teamCode})
-
           
-            if (team_Data) {
-               
+            if (team_Data) {               
                 const updated = await teamData.updateOne({ team: bidData.teamCode }, {
                     $set: { points: Number(team_Data.points) - Number(bidData.amount)},
                     $push: { questions: bidData.title }
@@ -92,7 +86,6 @@ io.on('connection', (socket) => {
         catch (err) {
             console.log("server side error")
         }
-
     })
 
 
@@ -130,11 +123,10 @@ app.post('/signUp', async (req, res) => {
 
 })
 
-let teamCode;
-
 app.get('/login',(req,res)=>{
     res.sendFile(__dirname + '/public/login.html');
 })
+
 app.post('/login', async (req, res) => {
     const userData = {
         teamCode: req.body.teamCode,
@@ -145,7 +137,6 @@ app.post('/login', async (req, res) => {
 
     if (authResult.success) {
         const loginTeamData = await teamData.findOne({team:userData.teamCode})
-        //console.log(loginTeamData);
         const list= await questionBank.find({})
         const newList=list.filter(question=>question.owner!=null)
         res.render('home', {userData:userData,points:loginTeamData.points,titleOwner:newList})
@@ -154,23 +145,6 @@ app.post('/login', async (req, res) => {
         res.send(authResult.message)
     }
 })
-
-// app.get('/home', async (req, res) => {
-//     const teamData = {
-//         totalPoints: 20,
-//         purse: 12,
-//         remainingTime: 59,
-//         currBidAmount: 6,
-//         problemPoints: 4,
-//         problemTitle: "Find minimum in rotated sorted array",
-//         problemDesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets  Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets  Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets  Aldus PageMaker including versions of Lorem Ipsum."
-//     }
-
-//     res.render('home', {
-//         teamCode,
-//         teamData
-//     });
-// })
 
 app.get('/hostLogin', (req, res) => {
     res.render('./hostLogin')
