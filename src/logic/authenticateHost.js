5e06f5bc5f87b16685ae184b8237163079a47727
Prakhar Verma <68277409+rxi-prakharvrm@users.noreply.github.com>
2024-09-02 16:48:51 +0530

    Update authenticateHost.js

--- src/logic/authenticateHost.js
+++ src/logic/authenticateHost.js
@@ -4,9 +4,9 @@ async function authenticateHost(user){
     try{
         const findUser = await hostData.findOne({username:user.username})
         if(!findUser || user.password!=findUser.password){
-            return {success:true,message:"valid credentials"}
+            return {success:false,message:"invalid creadentials"}
         }
-        return {success:false,message:"invalid creadentials"}
+        return {success:true,message:"valid credentials"}
     }
     catch(error){
         return {success:false,message:"interval server error"}
